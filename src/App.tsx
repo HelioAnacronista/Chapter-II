2b865976632b8edadc92631a5dec8bee32a00903
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2023-01-04 17:01:00 -0300

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
 
 export function App() {
   return (
-    <div className="App">
-
-      <h1>Hello</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
