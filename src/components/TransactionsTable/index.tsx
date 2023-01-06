4b5b8fb0491e0d50bc33f8df0a9bf5bb020b10df
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2023-01-06 15:09:59 -0300

    Configurando cliente do Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 import { Container } from './styled'
+import { api } from '../../services/api';
 
 export function TransactionsTable() {
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    api.get('/transactions')
+    .then(response => console.log(response.data))
   }, []);
 
   return (
